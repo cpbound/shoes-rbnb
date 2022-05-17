542f5de33c7366e7f3817f062f850e4586cfd83d
Bora Kim <miromiro@posteo.de>
2022-05-17 13:00:58 +0900

    fixed routes

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,4 +2,28 @@ class RentalsController < ApplicationController
   def index
     @rentals = Rental.all
   end
+
+  def new
+    @rental = Rental.new
+    @shoe = Shoe.find(params[:shoe_id])
+    # @user = User.find(params[:user_id])
+  end
+
+  def create
+    @rental = Rental.new(rental_params)
+    @shoe = Shoe.find(params[:shoe_id])
+    @rental.shoe = @shoe
+    if @rental.save
+      redirect_to shoe_rentals_path(@rental.shoe)
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def rental_params
+    params.require(:rental).permit(:start_date, :end_date, :shoe_id)
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :rentals, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  get "rentals", to: "rentals#index"
+  resources :shoes do
+    resources :rentals, only: [ :new, :create ]
+  end
 end

0df47ccee7a712c48e91b5afba01f0350b34991e
Reina Kitazawa <kreina1015@yahoo.co.jp>
2022-05-19 12:34:06 +0900

    add search function

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,7 +1,12 @@
 class ShoesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
   def index
-    @shoes = policy_scope(Shoe).order(created_at: :desc)
+    if params[:query].present?
+      sql_query = "color ILIKE :query OR category ILIKE :query OR size ILIKE :query"
+      @shoes = policy_scope(Shoe).search_by_color_and_category_and_size(params[:query])
+    else
+      @shoes = policy_scope(Shoe).order(created_at: :desc)
+    end
   end
 
   def show
@@ -9,5 +14,4 @@ class ShoesController < ApplicationController
     authorize @shoe
     @rental = Rental.new
   end
-
 end
--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -3,4 +3,11 @@ class Shoe < ApplicationRecord
   has_many :rentals, dependent: :destroy
   validates :name, :price, :size, presence: true
   has_one_attached :photo
+
+  include PgSearch::Model
+  pg_search_scope :search_by_color_and_category_and_size,
+    against: [ :color, :category, :size ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
